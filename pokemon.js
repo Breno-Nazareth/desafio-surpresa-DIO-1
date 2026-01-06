3983cd0e2d8f1017098ff33dc48c0a79a912830a
Breno Nazareth <53622462+Breno-Nazareth@users.noreply.github.com>
2026-01-06 09:25:19 -0300

    Add files via upload

---
+++ pokemon.js
@@ -0,0 +1,15 @@
+//Desafio surpresa do curso de Lógica de Programação do DIO.me
+//Utilizando apenas VETORES e OPERADORES ARITIMÉTICOS.
+
+let pokemon = ["Poochyena", "Zigzagoon", "Dragonite", "Dragonite", "Dragonite", "Poochyena", "Wurmple"]
+let level = ["2", "2", "5", "5", "5", "3", "2"]
+let gender = ["Masculino", "Feminino", "Masculino", "Feminino", "Feminino", "Feminino", "Masculino"]
+let hp = ["13/13", "13/13", "25/25", "24/24", "24/24", "15/15", "7/14"]
+
+console.log("O Pokemon " + pokemon[0] + " que está no nível " + level[0] + " e é do sexo " + gender [0] + ", está atual com " + hp[0] + " hp. Foi Cadastrado com Sucesso!")
+console.log("O Pokemon " + pokemon[1] + " que está no nível " + level[1] + " e é do sexo " + gender [1] + ", está atual com " + hp[1] + " hp. Foi Cadastrado com Sucesso!")
+console.log("O Pokemon " + pokemon[2] + " que está no nível " + level[2] + " e é do sexo " + gender [2] + ", está atual com " + hp[2] + " hp. Foi Cadastrado com Sucesso!")
+console.log("O Pokemon " + pokemon[3] + " que está no nível " + level[3] + " e é do sexo " + gender [3] + ", está atual com " + hp[3] + " hp. Foi Cadastrado com Sucesso!")
+console.log("O Pokemon " + pokemon[4] + " que está no nível " + level[4] + " e é do sexo " + gender [4] + ", está atual com " + hp[4] + " hp. Foi Cadastrado com Sucesso!")
+console.log("O Pokemon " + pokemon[5] + " que está no nível " + level[5] + " e é do sexo " + gender [5] + ", está atual com " + hp[5] + " hp. Foi Cadastrado com Sucesso!")
+console.log("O Pokemon " + pokemon[6] + " que está no nível " + level[6] + " e é do sexo " + gender [6] + ", está atual com " + hp[6] + " hp. Foi Cadastrado com Sucesso!")
